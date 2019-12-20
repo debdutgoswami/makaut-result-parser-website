eb10390df8106e3f8f563303c64ce7b8800f8c7a
Debdut Goswami <debdutgoswami@gmail.com>
2019-12-20 21:27:29 +0530

    ADDED features

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from flask import Flask, render_template, request, send_file, after_this_request
 app = Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
+@app.route('/home',methods=['GET','POST'])
 def home():
     if request.method == 'GET':
         return render_template('home.html')
@@ -35,5 +36,13 @@ def home():
         # sends the zip file. as_attachment parameters specify that the original name of the zip will be the 
         return send_file(filename)
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/howtouse')
+def howtouse():
+    return render_template('howtouse.html')
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
